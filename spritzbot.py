9a8e254b25f6906704558ee0dbd6b20d17e335d3
Harshad Sharma <harshad@harshadsharma.com>
2013-01-10 14:42:54 +0530

    Boilerplate code for streaming API.

--- spritzbot.py
+++ spritzbot.py
@@ -4,6 +4,10 @@
 import os
 import re
 import imp
+import json
+import requests
+
+from oauth_hook import OAuthHook
 
 class SpritzBot:
     """Twitter bot framework, loads extensions, connects via streaming
@@ -25,7 +29,23 @@ class SpritzBot:
                 extension = imp.load_module(name, *ext_info)
                 self.extensions[name] = extension
 
+    def start(self):
+        """Connects to Twitter's streaming API"""
+        OAuthHook.consumer_key = os.getenv('CONSUMER_KEY')
+        OAuthHook.consumer_secret = os.getenv('CONSUMER_SECRET')
+        oauth_hook = OAuthHook(os.getenv('ACCESS_TOKEN'),
+                               os.getenv('ACCESS_TOKEN_SECRET'))
+
+        session = requests.session(hooks={'pre_request':oauth_hook})
+        client = session.post('https://userstream.twitter.com/1.1/user.json',
+                              prefetch=False, verify=False)
+
+        for line in client.iter_lines(chunk_size=1, decode_unicode=True):
+            if line:
+                data = json.loads(line)
+                print data
 
 if __name__ == '__main__':
     bot = SpritzBot()
     print bot.extensions
+    bot.start()
