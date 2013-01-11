86747cfc29943d5acadfd43fa96a1148e750e81d
Harshad Sharma <harshad@harshadsharma.com>
2013-01-11 21:06:21 +0530

    Personalizing hello messages.

--- extensions/hello.py
+++ extensions/hello.py
@@ -3,7 +3,7 @@
 def process_mention(status, settings):
     print status.user.screen_name,':', status.text.encode('utf-8')
     if status.text.lower() == ('@%s hello' % settings.username):
-        return dict(response='Hello, world!')
+        return dict(response='Hello, %s!' % status.user.screen_name)
 
     if status.text.lower() == ('@%s unfollow' % settings.username):
         return dict(unfollow=True, response=u'If you insist… unfollowing.')
@@ -17,7 +17,7 @@ def process_follow(status, settings):
 def process_dm(status, settings):
     print status.direct_message.sender_screen_name,':', status.direct_message.text.encode('utf-8')
     if status.direct_message.text.lower() == 'hello':
-        return dict(dm='Hello, world!')
+        return dict(dm='Hello, %s!' % status.user.screen_name)
     else:
         return None
 
