a18d8fb5e11cdfb2db4e4c7ca603d602b51b7bb8
Harshad Sharma <harshad@harshadsharma.com>
2013-01-12 17:37:57 +0530

    Adding rudimentary suicide prevention measures.

--- extensions/tcemetery.py
+++ extensions/tcemetery.py
@@ -18,4 +18,26 @@ def process_dm(status, settings):
         if text[:2] == 'd ' or text[:2] == 'm ':
             text = text[2:]
 
+        trigger_words = ['suicide',
+                         'kill myself',
+                         'end my life',
+                         'end this life',
+                         'end it all',
+                         "don't want to live",
+                         'no reason to live',
+                         'unwilling to live',
+                         'why must I live',
+                         'should i live',
+                         'should i die',
+                         'want to die',
+                         'i could die',
+                         'end this life',
+                         'kill myself']
+
+        for word in trigger_words:
+            if word in text:
+                return dict(post=text, dm="I've posted that, but I'm "
+                            "concerned, are you alright? Talk to "
+                            "someone, talk to me.", delete_dm=True)
+
         return dict(post=text, delete_dm=True)
