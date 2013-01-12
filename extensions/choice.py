563847bab9f9f29765f803180c781d044938375a
Harshad Sharma <harshad@harshadsharma.com>
2013-01-12 10:22:42 +0530

    More unicode issues. Where's that handy tutorial again? Need to brush up on this again.

--- extensions/choice.py
+++ extensions/choice.py
@@ -2,8 +2,9 @@
 import random
 
 def process_mention(status, settings):
-    text = u'%' % status.text.lower().replace('@'+settings.username, '').strip()
+    text = status.text.lower().replace('@'+settings.username, '').strip()
+    text = text.encode('utf-8')
     tokens = text.split(' ')
     if tokens[0] == 'choose':
         choices = [x.strip() for x in ' '.join(tokens[1:]).split(',')]
-        return dict(response=random.choice(choices))
+        return dict(response=random.choice(choices).decode('utf-8'))
