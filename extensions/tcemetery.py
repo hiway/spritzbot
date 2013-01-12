f999ff9d6be5152829e2ac280e50546a29667f52
Harshad Sharma <harshad@harshadsharma.com>
2013-01-12 17:06:08 +0530

    Force unfollow is a stupid idea. Can't send more than one tweet w/o getting annoyed. Change the workflow.

--- extensions/tcemetery.py
+++ extensions/tcemetery.py
@@ -3,9 +3,12 @@
 # NOT SECURE, only an experiment, do NOT run this in production
 
 def process_follow(status, settings):
-    return dict(follow=True, dm='Ok, now tell me...')
+    return dict(follow=True, dm="Ok, now tell me... (simply DM me 'bye' so I'll unfollow)")
 
 
 def process_dm(status, settings):
     if status.direct_message.sender_screen_name != settings.username:
-        return dict(post=status.direct_message.text, delete_dm=True, force_unfollow=True)
+        if status.direct_message.text.lower().strip() == 'bye':
+            return dict(dm='Cya!', delete_dm=True, force_unfollow=True)
+
+        return dict(post=status.direct_message.text, delete_dm=True)
